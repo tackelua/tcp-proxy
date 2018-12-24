097e54629c90223b0efaf57f2aaeda9c431cb7fe
tackelua <tackelua@gmail.com>
2018-12-24 10:33:00 +0700

    add package.json

--- tcp-proxy.js
+++ tcp-proxy.js
@@ -1,11 +1,29 @@
 const TCPProxy = require('tcp-proxy.js');
-const proxy = new TCPProxy({ port: 5222 });
-proxy.createProxy({
+
+//Blynk hardware port
+const blynkHardware = new TCPProxy({ port: 5222 });
+blynkHardware.createProxy({
   forwardPort: 8442,
   forwardHost: 'localhost',
 });
-const proxy2 = new TCPProxy({ port: 5228 });
-proxy2.createProxy({
+
+//Blynk app port
+const blynkApp = new TCPProxy({ port: 5228 });
+blynkApp.createProxy({
   forwardPort: 9443,
   forwardHost: 'localhost',
+});
+
+//mqtt
+// const mqtt = new TCPProxy({ port: 443 });
+// mqtt.createProxy({
+//   forwardPort: 1883,
+//   forwardHost: 'localhost',
+// });
+
+//mqtt bridge
+const mqtt = new TCPProxy({ port: 443 });
+mqtt.createProxy({
+  forwardPort: 1883,
+  forwardHost: 'broker.shiftr.io',
 });
